d8587f48e2d4f0d33b7669504138648e586387e5
Roberto Garza <robertg4567@hotmail.com>
2018-08-06 18:04:43 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,16 +3,20 @@ class Owner
   attr_accessor :name, :pets
   def initialize(name)
     @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
   end
 
   def self.all
     @@all
   end
+  
   def self.count
     @@all.count
   end
+  
   def self.reset_all
     @@all = []
   end
+  
 end
