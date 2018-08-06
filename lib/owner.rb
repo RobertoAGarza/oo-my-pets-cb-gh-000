abee6db106f329986cd712b0bc2c9c704f58e44b
Roberto Garza <robertg4567@hotmail.com>
2018-08-06 18:30:47 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,7 +65,8 @@ class Owner
       end
       animals.clear
     end
-
   end
 
+def list_pets
+   "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
 end
